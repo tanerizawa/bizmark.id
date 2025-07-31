af04f9ee43ef7bc2998cb28e47d4af8418991ee9
tanerizawa <studiomalaka@gmail.com>
2025-07-31 07:50:45 +0700

    docs: improve documentation for roles decorator

--- backend/src/common/decorators/roles.decorator.ts
+++ backend/src/common/decorators/roles.decorator.ts
@@ -1,5 +1,13 @@
 import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '../../entities';
 
+/**
+ * Key used for roles metadata
+ */
 export const ROLES_KEY = 'roles';
+
+/**
+ * Decorator for specifying required roles to access a resource
+ * @param roles - Array of UserRole values required for access
+ */
 export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
